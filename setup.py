0059b1032d4ebb6c06f2bb27df3b6b71758fdee7
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-08-01 10:55:34 +0200

    updated to version 6.13

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ Programming Language :: Python :: 3
 """
 
 setup(name='ctcsound',
-      version='0.0.3',
+      version='6.13',
       url='https://github.com/fggp/ctcsound',
       description='Python bindings to the Csound API using ctypes', 
       classifiers=filter(None, classifiers.split('\n')),
       author='Francois Pinot',
       py_modules=['ctcsound'],
+      install_requires=[
+        'numpy>=1.16'
+      ]
 )
 
 
